774b90defd255a78901431de815924c0411e02be
Rob Fitzpatrick <robftz@gmail.com>
2011-06-15 20:27:50 +0100

    First commit, blank project

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    ('^ah/warmup$', 'djangoappengine.views.warmup'),
+
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+
+    ('', 'django.views.generic.simple.direct_to_template',
+     {'template': 'home.html'}),
+
+    #static assets (should be local-only)                   
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': 'media'}),
+)
